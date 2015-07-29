bb19309659a37ada5237c55eb6fd4094d99ef307
kerrin <kerrinenglish@gmail.com>
2015-07-30 09:13:35 +1000

    (build) add production config, and surge deployment

---
+++ webpack.config.production.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './scripts/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'autoprefixer-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+          loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          ]
+      }
+    ]
+  }
+};
