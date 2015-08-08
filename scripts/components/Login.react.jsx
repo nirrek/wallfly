154ab61fd514880dfced2888df7e482c98c560bd
kerrin <kerrinenglish@gmail.com>
2015-08-08 15:13:20 +1000

    (Login) use Api module & redirect on login

--- scripts/components/Login.react.jsx
+++ scripts/components/Login.react.jsx
@@ -1,54 +1,70 @@
-import React from 'react';
+var React = require('react');
 var axios = require('axios');
-var login = require('../utils/Login.js');
+var Api = require('../utils/Api.js');
+var Navigation = require('react-router').Navigation;
 
-class Login extends React.Component {
-  state = {
-    username: '',
-    password: '',
-  }
+/**
+ * Login View
+ * The login view allows a user to log into the service using their credentials.
+ */
+var Login = React.createClass({
+  mixins: [ Navigation ],
 
+  getInitialState() {
+    return {
+      username: '', // user entered username
+      password: '', // user entered password
+      authFailure: false, // did the authentication request fail
+    }
+  },
+
+  // Capture the input field state after each keypress.
   onChange(field, event) {
-    console.log('onChange: ', event.target.value);
+    this.setState({ [field]: event.target.value });
+  },
 
-    this.setState({
-      [field]: event.target.value
-    });
-  }
-
-  onSubmit = (event) => {
+  // Handle the form submission event when the user tries to log in.
+  onSubmit(event) {
     event.preventDefault();
     event.stopPropagation();
 
-    console.log(this.state);
+    Api.login({
+      username: this.state.username,
+      password: this.state.password,
+      callback: (err, response) => {
+        if (err) {
+          this.setState({ authFailure: true });
+          return;
+        }
 
-    axios.post('http://localhost:8000/login', {
-        username: this.state.username,
-        password: this.state.password,
-      })
-      .then((response) => {
-        login.setUser(response.data);
-        console.log(response);
-      })
-      .catch((response) => {
-        console.log(response);
-      });
-  }
+        // Transition to the propertyDetails view on successful login.
+        this.transitionTo('propertyDetails');
+      }
+    });
+  },
 
   render() {
-    let { username, password } = this.state;
+    var { username, password, authFailure } = this.state;
+    var authFailMessage = authFailure ? 'Username or password invalid' : null;
 
     return (
       <div>
         <h1>Login</h1>
-        <form onSubmit={this.onSubmit} action="http://localhost:8000/login" method="POST">
-          <input value={username} onChange={this.onChange.bind(this, 'username')} name="username" placeholder="username" />
-          <input value={password} onChange={this.onChange.bind(this, 'password')} name="password" placeholder="password" />
+        <form onSubmit={this.onSubmit}>
+          <div> { authFailMessage } </div>
+          <input value={username}
+                 onChange={this.onChange.bind(this, 'username')}
+                 name="username"
+                 placeholder="username" />
+          <input value={password}
+                 onChange={this.onChange.bind(this, 'password')}
+                 name="password"
+                 placeholder="password" />
           <button type="submit">Login</button>
         </form>
       </div>
     );
   }
-}
+});
 
 export default Login;
