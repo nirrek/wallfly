c5a42a70e1218e1abad3c6f7aaf64c2f09d9e3b6
kerrin <kerrinenglish@gmail.com>
2015-10-02 22:28:11 +1000

    Add chat-related components

---
+++ scripts/components/OwnerMessages.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var Property = require('../utils/Property.js');
+var Api = require('../utils/Api.js');
+var Chat = require('./Chat.jsx');
+var User = require('../utils/User.js');
+
+var Messages = React.createClass({
+  render() {
+    var user = User.getUser();
+    var sender = user.id;
+    var receiver = user.managingAgent;
+
+    return (
+      <div style={style.page}>
+        <Chat sender={sender} receiver={receiver} />
+      </div>
+    );
+  }
+});
+
+var style = {
+  page: {
+    display: 'flex',
+    flexDirection: 'column',
+  }
+};
+
+module.exports = Messages;
