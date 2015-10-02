e6c2ebfeb9da8903bee9424ef62df63ea7666b31
kerrin <kerrinenglish@gmail.com>
2015-10-02 22:28:54 +1000

    (Messages) consume the new chat components

--- scripts/components/Messages.jsx
+++ scripts/components/Messages.jsx
@@ -1,193 +1,37 @@
 var React = require('react');
-var User = require('../utils/User.js');
+var Property = require('../utils/Property.js');
 var Api = require('../utils/Api.js');
+var Chat = require('./Chat.jsx');
 
-var Avatar = React.createClass({
-  propTypes: {
-    avatarUrl: React.PropTypes.string
-  },
-
-  render() {
-    var { avatarUrl } = this.props;
-
-    if (!avatarUrl) {
-      avatarUrl = 'https://pbs.twimg.com/profile_images/479315519729070081/ty2LLr9m.jpeg';
-    }
-
-    return <img style={avatarStyle} width='40' height='40' src={avatarUrl} />;
-  }
-});
-
-var avatarStyle = {
-  borderRadius: 20,
-};
-
-var ChatMessage = React.createClass({
-  propTypes: {
-    avatarUrl: React.PropTypes.string,
-    message: React.PropTypes.string.isRequired,
-  },
-
-  render() {
-    var { avatarUrl, message, isUser } = this.props;
-
-    var containerStyle = Object.assign({},
-      msgStyle.container,
-      isUser ? msgStyle.containerUser : {},
-    );
-
-    var messageStyle = Object.assign({},
-      msgStyle.msg,
-      isUser ? msgStyle.msgUser : {},
-    );
-
-    var avatarWrapperStyle = Object.assign({},
-      msgStyle.avatarWrapper,
-      isUser ? msgStyle.avatarWrapperUser : {},
-    );
-
-    return (
-      <div style={containerStyle}>
-        <div style={avatarWrapperStyle}>
-          <Avatar avatarUrl={avatarUrl} />
-        </div>
-        <div style={messageStyle}>
-          {message}
-        </div>
-      </div>
-    );
-  }
-});
-
-var msgStyle = {
-  container: {
-    display: 'flex',
-    marginBottom: '1em',
-  },
-  avatarWrapper: {
-    marginRight: 15,
-    flexShrink: 0,
-  },
-  msg: {
-    backgroundColor: '#F5F5F5',
-    borderRadius: '20px',
-    fontSize: '13px',
-    color: '#222',
-    padding: '1em',
-    lineHeight: '1.5',
-  },
-  avatarWrapperUser: {
-    order: 1,
-    marginRight: 0,
-    marginLeft: 15,
-  },
-  msgUser: {
-    // backgroundColor: '#2ECC71',
-    // color: '#fff',
-  },
-  containerUser: {
-    justifyContent: 'flex-end',
-  },
-}
-
-// TODO, pull up the handler logic into a higher component
-var ChatPanel = React.createClass({
+var Messages = React.createClass({
   getInitialState() {
     return {
-      messages: [],
-      messageDraft: '',
-    }
+      sender: null,
+      receiver: null,
+    };
   },
 
   componentWillMount() {
-    this.sender = User.getUserId();
-    this.receiver = null;
-    this.getMessages(this.sender);
-  },
-
-  getMessages(callback) {
-    Api.getMessages({
-      callback: (err, response) => {
-        var { data } = response;
-
-        this.receiver = data.agent;
+    Api.getPropertyContacts({
+      propertyId: Property.getPropertyId(),
+      callback: (err, res) => {
+        console.log(res);
         this.setState({
-          messages: data.messages
-        });
-
-        if (callback) callback();
-      }
-    })
-  },
-
-  onSend(event) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    Api.postMessages({
-      data: {
-        sender: this.sender,
-        receiver: this.receiver,
-        message: this.state.messageDraft,
-      },
-      callback: (err, response) => {
-        if (err) {
-          // TODO
-          return;
-        }
-
-        // Refetch all messages from the server.
-        this.getMessages(() => {
-          this.setState({ messageDraft: '' });
+          sender: res.data.tenantId,
+          receiver: res.data.agentId,
         });
       }
-    })
-  },
-
-  onChange(event) {
-    this.setState({ messageDraft: event.target.value });
-  },
-
-  render() {
-    var { messages, messageDraft } = this.state;
-
-    var messageComponents = messages.map(message => {
-      var isUser = message.sender === User.getUserId();
-      var avatar = isUser ? 'https://avatars3.githubusercontent.com/u/1008618?v=3&s=460'
-                          : 'https://s3.amazonaws.com/uifaces/faces/twitter/jm_denis/48.jpg'
-      return <ChatMessage key={message.id} isUser={isUser} avatarUrl={avatar} message={message.message} />;
     });
+  },
 
-    return (
-      <div>
-        <div>
-          {messageComponents}
-        </div>
-        <div style={{display: 'flex'}}>
-          <textarea value={messageDraft} onChange={this.onChange} style={panelStyle.textarea} />
-          <button onClick={this.onSend}>Send</button>
-        </div>
-      </div>
-    );
-  }
-});
-
-var panelStyle = {
-  textarea: {
-    borderRadius: 5,
-    borderColor: '#ddd',
-    flexGrow: 1,
-    marginRight: 15,
-    padding: '1em',
-  }
-};
-
-var RepairRequest = React.createClass({
   render() {
+    var { sender, receiver } = this.state;
+
     return (
       <div style={style.page}>
-        <h3>Chat with Mr Agent</h3>
-        <ChatPanel/>
+        {sender ? (
+          <Chat sender={sender} receiver={receiver} />
+        ) : null}
       </div>
     );
   }
@@ -197,8 +41,7 @@ var style = {
   page: {
     display: 'flex',
     flexDirection: 'column',
-    padding: '20px',
   }
 };
 
-module.exports = RepairRequest;
+module.exports = Messages;
