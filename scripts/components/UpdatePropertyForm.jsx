8a6b345bbfd327e98e6a1afc5d178b200a121873
jListo <joseph.listo@gmail.com>
2015-10-05 18:45:01 +1000

    Update Property Expansion
    Update the update property form to include more fields and fill fields
    with current values.

--- scripts/components/UpdatePropertyForm.jsx
+++ scripts/components/UpdatePropertyForm.jsx
@@ -13,15 +13,36 @@ var ErrorMessage = require('./ErrorMessage.jsx');
 var UpdatePropertyForm = React.createClass({
   propTypes: {
     propertyDetailsUpdated: React.PropTypes.func,
-    propertyID: React.PropTypes.number,
+    propertyID: React.PropTypes.string,
   },
 
   getInitialState() {
     return {
+      street: '', // Property street address
+      suburb: '', // Property suburb
+      postcode: '', // Property post code
+      ownerEmail: '', // User entered owner email
       tenantEmail: '', // User entered tenant email
-      image: '', // base64 encoding of the user selected image.
+      photo: '', // base64 encoding of the user selected image.
       fileSizeError: '', // file size error message
     }
+  },
+
+  componentWillMount() {
+    this.getPropertyDetails();
+  },
+
+  // Fetches the details for the property
+  getPropertyDetails() {
+    var propertyId = parseInt(this.props.propertyID);
+
+    Api.getPropertyDetails({
+      propertyId,
+      callback: (err, response) => {
+        if (err) return console.log(err);
+        this.setState(response.data);
+      }
+    });
   },
 
   onButtonClick() {
@@ -43,12 +64,17 @@ var UpdatePropertyForm = React.createClass({
   onSubmit(event) {
 
     // API call to update property details
-    var propertyId = this.props.propertyID;
+    var propertyId = parseInt(this.props.propertyID);
+
     Api.updatePropertyDetails({
       data: {
+        propertyId: propertyId,
         tenantEmail: this.state.tenantEmail,
-        propertyId: this.props.propertyID,
-        image: this.state.image,
+        ownerEmail: this.state.ownerEmail,
+        street: this.state.street,
+        suburb: this.state.suburb,
+        postcode: this.state.postcode,
+        photo: this.state.photo,
       },
       callback: (err, response) => {
         if (err) {
@@ -56,11 +82,8 @@ var UpdatePropertyForm = React.createClass({
           return;
         }
 
-        // Clear the form
-        this.setState({
-          tenantEmail: '',
-          image: '',
-        });
+        // Request new values for the form
+        this.getPropertyDetails();
 
         this.props.propertyDetailsUpdated();
         this.refs.dialog.dismiss();
@@ -69,7 +92,7 @@ var UpdatePropertyForm = React.createClass({
   },
 
   onImageSelected(payload) {
-    this.setState({ image: payload.dataURL });
+    this.setState({ photo: payload.dataURL });
   },
 
   onImageSizeError(error) {
@@ -80,7 +103,7 @@ var UpdatePropertyForm = React.createClass({
   },
 
   render() {
-    var { tenantEmail, fileSizeError } = this.state;
+    var { street, suburb, postcode, ownerEmail, tenantEmail, fileSizeError } = this.state;
     var sizeError = fileSizeError ? (
       <ErrorMessage fillBackground={true}>Error: {fileSizeError}</ErrorMessage>
     ) : null;
@@ -102,14 +125,38 @@ var UpdatePropertyForm = React.createClass({
           ref="dialog">
           <div style={style.error}> { errorMessage } </div>
           <TextField
+            value={street}
+            multiLine={true}
+            name="Street Address"
+            onChange={this.onChange.bind(this, 'street')}
+            floatingLabelText="Street Address" />
+          <TextField
+            value={suburb}
+            multiLine={true}
+            name="Suburb"
+            onChange={this.onChange.bind(this, 'suburb')}
+            floatingLabelText="Suburb" />
+          <TextField
+            value={postcode}
+            multiLine={true}
+            name="Postcode"
+            onChange={this.onChange.bind(this, 'postcode')}
+            floatingLabelText="Postcode" />
+          <TextField
+            value={ownerEmail}
+            multiLine={true}
+            name="Owner Email"
+            onChange={this.onChange.bind(this, 'ownerEmail')}
+            floatingLabelText="Owner Email" />
+          <TextField
             value={tenantEmail}
             multiLine={true}
             name="Tenant Email"
             onChange={this.onChange.bind(this, 'tenantEmail')}
-            floatingLabelText="Tenant Email"
+            floatingLabelText="Tenant Email (Leaving blank will remove current tenant)"
             fullWidth />
           <div style={style.selectorContainer}>
-            <Label>Image</Label>
+            <Label>Image (Not Required)</Label>
             {sizeError}
             <ImageSelector maxSize={200000}
                            onImageSelected={this.onImageSelected}
