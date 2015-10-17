237d841682379f65366b139e6b8c4d14f25fd326
kerrin <kerrinenglish@gmail.com>
2015-10-17 15:39:07 +1000

    (OwnerRepairRequests) add status update selector, add priority

--- scripts/components/OwnerRepairRequests.jsx
+++ scripts/components/OwnerRepairRequests.jsx
@@ -1,14 +1,20 @@
 var React = require('react');
 var Api = require('../utils/Api.js');
 var moment = require('moment');
-var OwnerRepairRequestStatus = require('./OwnerRepairRequestStatus.jsx');
 var Radium = require('radium');
 var PageHeading = require('./PageHeading.jsx');
+var Priority = require('./Priority.jsx');
+var User = require('../utils/User.js');
+var MuiContextified = require('./MuiContextified.jsx');
+var MaterialUi = require('material-ui');
+var SelectField = MaterialUi.SelectField;
+var Snackbar = MaterialUi.Snackbar;
 
 var OwnerRepairRequests = React.createClass({
   getInitialState() {
     return {
-      requests: []
+      requests: [],
+      responseReceived: false, // received API response?
     };
   },
 
@@ -18,7 +24,10 @@ var OwnerRepairRequests = React.createClass({
       propertyId,
       callback: (err, response) => {
         if (err) return console.log(err);
-        this.setState({ requests: response.data });
+        this.setState({
+          responseReceived: true,
+          requests: response.data
+        });
       }
     });
   },
@@ -27,20 +36,48 @@ var OwnerRepairRequests = React.createClass({
     this.getPropertyRepairRequests();
   },
 
+  onStatusChange(id, event, selectedStatusIndex) {
+    var modifiedRequest = this.state.requests.find(el => el.id === id);
+    modifiedRequest.status = statuses[selectedStatusIndex].text;
+
+    Api.putRepairRequest({
+      data: {
+        ...modifiedRequest
+      },
+      callback: (err, res) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        this.refs.snackbar.show();
+      }
+    });
+
+    this.forceUpdate();
+  },
+
   render() {
+    // Don't render until we have data cached from the server.
+    if (!this.state.responseReceived) return null;
+
     var rows = this.state.requests.map(request => {
       return (
         <tr key={request.id}>
           <td>{moment(request.date).format('Do MMM YYYY')}</td>
+          <td><Priority type={request.priority} /></td>
           <td>{request.request}</td>
           <td><img style={styles.img} src={request.photo} /></td>
-          <td>{request.status}</td>
           <td>
-            <OwnerRepairRequestStatus
-                requestId={request.id}
-                propertyId={this.props.params.propertyId}
-                statusChanged={this.getPropertyRepairRequests}
-                status={request.status} />
+            { User.getUser().type === 'agent' ? (
+              <SelectField
+                value={request.status}
+                valueMember="name"
+                onChange={this.onStatusChange.bind(this, request.id)}
+                menuItems={statuses} />
+            ) : (
+              request.status
+            )}
           </td>
         </tr>
       );
@@ -53,19 +90,32 @@ var OwnerRepairRequests = React.createClass({
           <thead>
             <colgroup style={{width: 140}}></colgroup>
             <th>Date</th>
+            <th>Priority</th>
             <th>Request</th>
             <th>Photo</th>
             <th>Status</th>
-            <th></th>
           </thead>
           <tbody>
             {rows}
           </tbody>
         </table>
+
+        <Snackbar
+          ref="snackbar"
+          message="Repair Request Updated"
+          autoHideDuration={3000} />
       </div>
     );
   }
 });
+
+// Statuses
+var statuses = [
+  { name: 'Submitted', text: 'Submitted' },
+  { name: 'Pending', text: 'Pending' },
+  { name: 'Approved', text: 'Approved' },
+  { name: 'Declined', text: 'Declined' },
+];
 
 var styles = {
   container: {
@@ -78,4 +128,4 @@ var styles = {
   },
 };
 
-module.exports = Radium(OwnerRepairRequests);
+module.exports = MuiContextified(Radium(OwnerRepairRequests));
