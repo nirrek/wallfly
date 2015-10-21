bc322474cf4dd05dcbec23f9fe9bb24eb31b3b1b
kerrin <kerrinenglish@gmail.com>
2015-10-21 22:49:30 +1000

    Add Status component for payment statuses

---
+++ scripts/components/Status.jsx
@@ -0,0 +1,50 @@
+var React = require('react');
+var Radium = require('radium');
+
+/**
+ * Status label
+ */
+var Status = React.createClass({
+  propTypes: {
+    type: React.PropTypes.string, // ['red', 'green', 'yellow', 'blue']
+    children: React.PropTypes.node,
+  },
+  render() {
+    return (
+       <span style={[
+           styles.status,
+           styles[this.props.type]
+         ]}>{this.props.children || this.props.type}</span>
+    );
+  }
+});
+
+var styles = {
+  status: {
+    backgroundColor: '#ECECEC',
+    color: '#868686',
+    padding: '3px 8px',
+    borderRadius: 4,
+    display: 'inline-block',
+    whiteSpace: 'nowrap',
+    fontSize: 14,
+  },
+  red: {
+    backgroundColor: '#F65035',
+    color: '#5F1509',
+  },
+  yellow: {
+    backgroundColor: 'rgba(253, 205, 42, 0.81)',
+    color: '#6D5913',
+  },
+  blue: {
+    backgroundColor: '#A9DDF9',
+    color: '#26536B',
+  },
+  green: {
+    backgroundColor: '#2ECC71',
+    color: '#0E4A0E',
+  }
+};
+
+module.exports = Radium(Status);
