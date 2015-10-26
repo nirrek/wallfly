2527f4344ba9147428187af446caf64bc975a0c4
kerrin <kerrinenglish@gmail.com>
2015-10-26 20:19:18 +1000

    Add Screenshot component for use in the guides

---
+++ scripts/components/Screenshot.jsx
@@ -0,0 +1,42 @@
+var React = require('react');
+var Radium = require('radium');
+
+var Screenshot = React.createClass({
+  propTypes: {
+    src: React.PropTypes.string.isRequired,
+    caption: React.PropTypes.string.isRequired,
+  },
+
+  render() {
+    var { src, caption } = this.props;
+
+    return (
+      <div style={styles.pocket}>
+        <img style={styles.img} src={require(`../../assets/${src}`)} />
+        <div style={styles.caption}>
+          {caption}
+        </div>
+      </div>
+    );
+  }
+});
+
+var styles = {
+  pocket: {
+    padding: '1em',
+    borderRadius: 4,
+    background: '#f0f0f0',
+  },
+  img: {
+    width: '100%',
+    borderRadius: 4,
+  },
+  caption: {
+    fontSize: 11,
+    color: '#555',
+    textAlign: 'center',
+    paddingTop: '1em',
+  },
+};
+
+module.exports = Radium(Screenshot);
