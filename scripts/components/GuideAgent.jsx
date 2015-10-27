b72e13248d2445ac25e249e3fe44ed0390b1460f
kerrin <kerrinenglish@gmail.com>
2015-10-27 22:29:49 +1000

    Fix tonnes of written mistakes

--- scripts/components/GuideAgent.jsx
+++ scripts/components/GuideAgent.jsx
@@ -41,42 +41,42 @@ var GuideAgent = React.createClass({
 
           <div style={styles.section}>
             <h2 style={styles.heading}>Calendar (Global)</h2>
-            <p>This calendar shows a list of all your appointments that have been added to various properties. By clicking the appointment you can view further details. You can also edit or delete a giving property by clicking the more options icon on the right of every appointment.</p>
+            <p>This calendar shows a list of all your events that have been added to various properties. By clicking the event you can view further details. You can also edit or delete a given event by clicking the more options icon on the right of every event.</p>
             <Screenshot src='agent-calendar.png'
-                        caption='All your appointments are aggregated into one calendar list.' />
+                        caption='All your events are aggregated into one calendar list.' />
           </div>
 
           <div style={styles.section}>
             <h2 style={styles.heading}>Repairs (Global)</h2>
-            <p>Repair requests from all properties that have been submitted are listed. Every repair request is categoried into priorties which can be used to filter the result. This can be done using the Filter by Priorty dropdown menu. As an agent, you can also  change the status of any given repair request.</p>
+            <p>Repair requests from all properties that have been submitted are listed. Every repair request is categorized into priorties which can be used to filter the result. This can be done using the Filter by Priorty dropdown menu. As an agent, you can also  change the status of any given repair request.</p>
             <Screenshot src='agent-repair-requests.png'
-                        caption='All your appointments are aggregated into one calendar list.' />
+                        caption='An aggregated list of repair requests across all properties you manage.' />
           </div>
 
           <div style={styles.section}>
             <h2 style={styles.heading}>Overdue Rent</h2>
             <p>All overdue rent payments across all properties are listed. You can proceed to communicate with the tenant to rectify the issue.</p>
             <Screenshot src='agent-overduerent.png'
-                        caption='List all overdue rend in one place.' />
+                        caption='Lists all overdue rent in one place.' />
           </div>
 
           <div style={styles.section}>
             <h2 style={styles.heading}>Property Details</h2>
-            <p>The property details gives an overview of all the important details of your property, including details for your managing agent, your current listed details, and the date that your lease expires. You can also update the details of the properties.</p>
+            <p>The property details pages gives an overview of all the main details of a property, including details of the owner, the current tenant, and when the current lease expires. You can also easily update the details of the properties on this page.</p>
             <Screenshot src='agent-property-details.png'
                         caption='Property details page.' />
           </div>
 
           <div style={styles.section}>
             <h2 style={styles.heading}>Payments</h2>
-            <p>The payments page allows you to see all rent payments for that property. You can also add a payment that you've received here. </p>
+            <p>The payments page allows you to see all payments associated with a particular property. Typically this will be a mix of rent payments from the tenant, payments you or the owner have made for repairs, and charges you have issued to the owner (including your monthly fees). The Add Payment functionality on this page allows you to add charges to the payment log for you and the owner to see (these are typically payments for repairs, or fees you have chared the owner). Importantly, these extra payments will not be visible by the tenant, they will only see their rent payments. </p>
             <Screenshot src='agent-payments.png'
                         caption='Payments page.' />
           </div>
 
           <div style={styles.section}>
             <h2 style={styles.heading}>Repair Requests</h2>
-            <p>The repair requests page allows you to see all requests lodged by the tenants. You can update the status of each requests to pending, approved or declined. The status will then be updated for the tenants to check. </p>
+            <p>The repair requests page allows you to see all requests lodged by tenants of the properties that you manage. You can update the status of each requests to pending, approved or declined. The status will then be updated for the tenants to check. </p>
             <Screenshot src='agent-repair-requests-property.png'
                         caption='Repair requests page.' />
           </div>
@@ -90,14 +90,14 @@ var GuideAgent = React.createClass({
 
           <div style={styles.section}>
             <h2 style={styles.heading}>Calendar</h2>
-            <p>The calendar page allows you to easily see any important dates for the property. You can add events and appointments that will be shown on the calendar include days that repairmen may be coming to fix something, inspections days, and the expiry of the lease. On a particular calendar day you can see a small snippet for any events, to get full information for the event simply click on the event to open up a more detailed view of it.</p>
+            <p>The calendar page allows you to easily see any important dates for the property. You can add events and events that will be shown on the calendar include days that repairmen may be coming to fix something, inspections days, and the expiry of the lease. On a particular calendar day you can see a small snippet for any events, to get full information for the event simply click on the event to open up a more detailed view of it.</p>
             <Screenshot src='agent-calendar-property.png'
                         caption='Calendar Page' />
           </div>
 
           <div style={styles.section}>
             <h2 style={styles.heading}>Messages</h2>
-            <p>The messages page give you the ability to chat with your owners and tenants. No need to keep track of emails or text messages between you and your agent. Using the messages feature ensures there is a record of everything you and your clients have ever discussed. It's quick and easy, and your agent will be in the Wallfly app all the time, so you will get a swift response.</p>
+            <p>The messages page give you the ability to chat with your owners and tenants. No need to keep track of emails or text messages between you and your clients/tenant. Using the messages feature ensures there is a record of everything you and your clients/tenants have ever discussed.</p>
             <Screenshot src='agent-messages.png'
                         caption='Messages Page' />
           </div>
